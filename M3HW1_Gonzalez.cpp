beaa00f1a3ad3db3ad380d0230d1b20097871179
EdithGonz <gonzalee4960@student.faytechcc.edu>
2025-03-10 06:29:55 +0000

    WORKING

--- M3HW1_Gonzalez.cpp
+++ M3HW1_Gonzalez.cpp
@@ -7,38 +7,43 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main() 
-{
-  string response1, response2, response3, response4;
-  cout << "Welcome to Mcdonalds, what can I get you?" << endl;
-  getline(cin, response1);
-  cout << "Anything else with that order?" << endl;
-  getline(cin, response2);
-  cout << "Anything else?" << endl;
-  getline(cin, response3);
-  cout << "That will be $10 pull up to the next window." << endl;
-  getline(cin, response4);
-  
-  
+
+void task1() {
+
+    /* QUESTION 1 */
+    string response1, response2, response3;
+    cout << "Welcome to Mcdonalds, what can I get you?" << endl;
+    getline(cin, response1);
+    cout << "Anything else with that order?" << endl;
+    getline(cin, response2);
+    cout << "That will be $10 pull up to the next window." << endl;
+    getline(cin, response3);
+}
+
+void task2() {
+
+    /* QUESTION 2 */
+    cout << "Hello" << endl;
+
+}
+
+void task3() {
+    cout << "World" << endl;
+}
+
+void task4() {
+    cout << "LMAO" << endl;
+}
+
+int main () {
+    task2();
+    task1();
+    task3();
+    task4();
+
+
+
+
   return 0; 
 
 } 
-
-
-//QUESTION 2
-#include <iostream>
-int main ()
-{
-
-
-
-
-
-
-
-
-
-
-
-    return 0
-}
