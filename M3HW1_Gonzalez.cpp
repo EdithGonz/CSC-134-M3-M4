c8f663b7b2bb769a1bba40f52f5c9b244a186660
EdithGonz <gonzalee4960@student.faytechcc.edu>
2025-03-10 05:14:59 +0000

    M3HW1 In Progress

---
+++ M3HW1_Gonzalez.cpp
@@ -0,0 +1,44 @@
+//CSC 134
+//M3HW1--Gold
+//Edith Gonzalez
+//03-10-25
+
+//QUESTION 1
+#include <iostream>
+#include <string>
+using namespace std;
+int main() 
+{
+  string response1, response2, response3, response4;
+  cout << "Welcome to Mcdonalds, what can I get you?" << endl;
+  getline(cin, response1);
+  cout << "Anything else with that order?" << endl;
+  getline(cin, response2);
+  cout << "Anything else?" << endl;
+  getline(cin, response3);
+  cout << "That will be $10 pull up to the next window." << endl;
+  getline(cin, response4);
+  
+  
+  return 0; 
+
+} 
+
+
+//QUESTION 2
+#include <iostream>
+int main ()
+{
+
+
+
+
+
+
+
+
+
+
+
+    return 0
+}
