7babffd87a5705afb3df4c20e2b8dd01467f4460
EdithGonz <gonzalee4960@student.faytechcc.edu>
2025-04-07 02:55:20 +0000

    Done!

--- M4LAB1_Gonzalez.cpp
+++ M4LAB1_Gonzalez.cpp
@@ -9,20 +9,11 @@ using namespace std;
 int main() 
 {
   int width, height;
-  width = 5;
-  height = 5;
+  width = 10;
+  height = 8;
 
-  // one row
-  for (int i=0; i < width; i++) {
-    cout << "*" << " ";
-  }
-  cout << endl;
 
-  // one column
-  for (int j=0; j < height; j++) {
-    cout << "*" << endl;
-  }
-  
+
   for (int j=0; j < height; j++) {
     for (int i=0; i < width; i++) {
         cout << "*" << " ";
